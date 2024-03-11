74492430e9a84cda7a040ec6b9e8148a8b0cedc2
barabarinov <barabarinov@gmail.com>
2024-03-11 14:40:07 +0200

    added forms, updated models, views

--- tasks/views.py
+++ tasks/views.py
@@ -1,36 +1,49 @@
+from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
+from tasks.forms import TaskForm
 from tasks.models import Task, Tag
 
 
 class TaskListView(ListView):
     model = Task
-    ordering = ['-created_at', "is_done"]
+    ordering = ["-created_at", "is_done"]
 
 
 class TaskCreateView(CreateView):
     model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("tasks:index")
 
 
 class TaskUpdateView(UpdateView):
     model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("tasks:index")
 
 
 class TaskDeleteView(DeleteView):
     model = Task
+    success_url = reverse_lazy("tasks:index")
 
 
 class TagListView(ListView):
     model = Tag
+    ordering = ["name"]
 
 
 class TagCreateView(CreateView):
     model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:tag-list")
 
 
 class TagUpdateView(UpdateView):
     model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:tag-list")
 
 
 class TagDeleteView(DeleteView):
     model = Tag
+    success_url = reverse_lazy("tasks:tag-list")
